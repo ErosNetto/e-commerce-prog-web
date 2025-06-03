09dabeec8d584c2790e0ad0e2a7062900bc3269b
Eros Netto Antunes <113219307+ErosNetto@users.noreply.github.com>
2025-06-03 18:40:01 -0300

    Att 01/06

---
+++ cantinhoVerdeMVC/app/controllers/carrinhoController.php
@@ -0,0 +1,9 @@
+<?php
+
+class CarrinhoController extends Controller
+{
+  public function index()
+  {
+    $this->view('carrinho');
+  }
+}
---
+++ cantinhoVerdeMVC/app/controllers/contaController.php
@@ -0,0 +1,9 @@
+<?php
+
+class ContaController extends Controller
+{
+  public function index()
+  {
+    $this->view('conta');
+  }
+}
